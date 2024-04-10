ce5dd31169753fe23d4c810fb7c7e6ede6c42138
流氓兔欸 <84904112+chao-code@users.noreply.github.com>
2024-04-10 11:38:01 +0800

    Create 1_binary_search.cpp

---
+++ Search Algorithm/1_binary_search.cpp
@@ -0,0 +1,25 @@
+bool check(int x) {/* ... */} // 检查x是否满足某种性质
+
+//适用情况：区间[l,r]被划分成[l, mid]和[mid+1, r]
+int bin_search_1(int l, int r){
+	while(l < r){
+		int mid = l + r >> 1;
+		if(check(mid))
+			r = mid;
+		else
+			l = mid + 1;
+	}
+	return l;
+}
+
+//使用情况：区间[l,r]被划分成[l, mid-1]和[mid, r]
+int bin_search_2(int l, int r){
+	while(l < r){
+		int mid = l + r + 1 >> 1;
+		if(check(mid))
+			l = mid;
+		else
+			r = mid - 1;
+	}
+	return l;
+}
